c85fe8fd8848b2803821f0d2b1e325f68ed7f657
Nishant Raj <115339821+NishantRaj278@users.noreply.github.com>
2025-07-01 22:07:15 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chat-nest-api-eight.vercel.app",
+  baseURL: "https://chat-nest-api-eight.vercel.app/api",
   withCredentials: true,
 });
