a1b73e66e9fdcdd9ef1275f55003e7d6db90bc7c
Nishant Raj <rnishant721@gmail.com>
2025-07-15 23:03:29 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,9 @@ import connectDB from "./config/mongoose-connection.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { server, app } from "./config/socket.js";
-import path from "path";
 dotenv.config();
+
+connectDB();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
@@ -24,19 +25,10 @@ app.get("/", (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-const __dirname = path.resolve();
 
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
-
 server.listen(port, () => {
   console.log(`server running on port ${port}`);
-  connectDB();
 });
